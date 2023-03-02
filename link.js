a703341e2ef3e8c759a27f9c60e283351146f69b
DookieeFox <126732215+DookieeFox@users.noreply.github.com>
2023-03-01 21:29:49 -0300

    Add files via upload

---
+++ link.js
@@ -0,0 +1,10 @@
+function redireccion() {
+    location.href= "https://www.youtube.com/watch?v=OLshMHuxGpo&t=4s"
+    
+}
+
+
+function portadadiario() {
+    location.href= "https://www.perfil.com/noticias/gaming/un-argentino-llego-al-mundial-de-street-fighter-peek.phtml"
+    
+}
